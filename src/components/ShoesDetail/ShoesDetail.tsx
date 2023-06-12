289dae1c20740929edb415fa5a91c9061c002374
Backflipper666 <elderly619@gmail.com>
2023-06-13 04:44:55 +0600

    change image to image2 when on hover

--- src/components/ShoesDetail/ShoesDetail.tsx
+++ src/components/ShoesDetail/ShoesDetail.tsx
@@ -1,12 +1,26 @@
 import { Shoe } from '../../interfaces/shoe';
 import nextId from 'react-id-generator';
 import './ShoesDetail.scss';
+import { useState } from 'react';
 const ShoesDetail = ({ shoe }: { shoe: Shoe }) => {
+  const [hovered, setHovered] = useState(false);
+  const [currentImage, setCurrentImage] = useState(
+    btoa(String.fromCharCode(...new Uint8Array(shoe.image.data.data)))
+  );
+
   const images = [];
   // images.push(shoe.image);
   if (shoe.image2) images.push(shoe.image2);
   if (shoe.image3) images.push(shoe.image3);
   if (shoe.image4) images.push(shoe.image4);
+
+  const handleMouseEnter2ndImage = (image: any) => {
+    setCurrentImage(image);
+  };
+
+  const handleMouseLeave2ndImg = (image: any) => {
+    setCurrentImage(image);
+  };
 
   const { contentType, data } = shoe.image || {};
 
@@ -16,16 +30,31 @@ const ShoesDetail = ({ shoe }: { shoe: Shoe }) => {
       String.fromCharCode(...new Uint8Array(shoe.image.data.data))
     );
 
+    const base64StringImg2 = shoe.image2
+      ? btoa(String.fromCharCode(...new Uint8Array(shoe.image2.data.data)))
+      : '';
+
     // Convert the Uint8Array image data to a base64-encoded string
 
     return (
       <div className="card">
         <div className="card__wrapper">
+          <div className="card__span-wrapper">
+            {' '}
+            <span className="card__indicator card__indicator-1"></span>
+            <span
+              className="card__indicator card__indicator-2"
+              onMouseEnter={() => handleMouseEnter2ndImage(base64StringImg2)}
+              onMouseLeave={() => handleMouseLeave2ndImg(base64String)}
+            ></span>
+            <span className="card__indicator card__indicator-3"></span>
+            <span className="card__indicator card__indicator-4"></span>
+          </div>
           <p className="card__ad card__promotion">NEW</p>
           <p className="card__discount card__promotion">-20 %</p>
           <span className="card__heart"></span>
           <img
-            src={`data:image/png;base64,${base64String}`}
+            src={`data:image/png;base64,${currentImage}`}
             width="300px"
             className="card__image-first"
           />
