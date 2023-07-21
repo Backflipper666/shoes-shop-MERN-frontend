bb4f9606139a09d14f1c5a390f7a67fa7138f32b
Backflipper666 <elderly619@gmail.com>
2023-07-22 04:06:11 +0600

    create AdminShoes component

---
+++ src/pages/AdminShoes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShoesList from '../components/ShoesList/ShoesList';
+
+const AdminShoes = () => {
+  return (
+    <div>
+      <ShoesList />
+    </div>
+  );
+};
+
+export default AdminShoes;
